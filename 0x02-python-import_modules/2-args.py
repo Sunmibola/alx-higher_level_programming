b3b12085049bbb5bf49606fc330c9a2775417e14
Sunmibola <olaniyisunmibola@gmail.com>
2022-12-02 04:06:49 -0800

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,12 +3,8 @@ import sys
 
 
 if __name__ == '__main__':
-    """Prints the argument list passed to the program
+    """Prints the argument list passed to the program"""
 
-    The program takes all the arguments starting from the second
-    and prints the number of arguments and their value
-
-    """
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -17,5 +13,6 @@ if __name__ == '__main__':
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-        for i in range(count):
+
+    for i in range(count):
             print("{}: {}".format(i + 1, sys.argv[i + 1]))
